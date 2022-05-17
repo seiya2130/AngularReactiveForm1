60304159679bde1a36256fc9a00fb9b1f7f87b3e
Seiya_Satoh <satohseiya0312@gmail.com>
2022-05-17 23:31:10 +0900

    トップ画面、フォーム作成画面追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewFormComponent } from './new-form/new-form.component';
+import { TopComponent } from './top/top.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/top', pathMatch: 'full' },
+  { path: 'top', component: TopComponent },
+  { path: 'newform', component: NewFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewFormComponent } from './new-form/new-form.component';
+import { TopComponent } from './top/top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewFormComponent,
+    TopComponent
   ],
   imports: [
     BrowserModule,
